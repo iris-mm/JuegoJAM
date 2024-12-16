fedd04ce7ef5e4647fcb5a9adaf3250f722028a0
Lumi151 <l.lorente.2023@alumnos.urjc.es>
2024-12-16 18:45:56 +0100

    .

---
+++ JuegoPEZ/Assets/Scripts/Póker/MostrarPremio.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using TMPro; // Necesario si usas TextMeshPro
+
+public class MostrarPremio : MonoBehaviour
+{
+    public TextMeshProUGUI textoPremio; // Arrastra aquí el componente del texto desde el editor
+
+    // Método para actualizar el texto en pantalla
+    public void ActualizarTextoPremio(string mensaje)
+    {
+        textoPremio.text = mensaje;
+    }
+}
